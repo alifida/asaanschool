908421532e3058d4b0602c270fea78bec49c91e2
alifida <alifida.86@gmail.com>
2024-05-26 23:37:35 +0500

    Update index.php

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ define('RELEASE_VERSION', '2.0.2');
  * installed
  * NOTE: If you change these, also change the error_reporting() code below
  */
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
  
 /*
  * ---------------------------------------------------------------
